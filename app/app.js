d23cebd11ba61ac56aa18a9cafc84324e6ace411
Steve Belovarich <steve@installationspace.com>
2014-12-07 15:31:51 -0800

    Changed the order of loading SCSS, moved respond to main.js

--- app/app.js
+++ app/app.js
@@ -15,15 +15,6 @@
 
       var app, appName = 'NewApp';
 
-      // create an instance of res.js globally
-      var respond = new res([{"state": "mobile","breakpoint": 320,"cols": 4,"margin": 10,"gutter": 10},
-                       {"state": "phablet","breakpoint": 568,"cols": 4,"margin": 10,"gutter": 10},
-                       {"state": "tablet","breakpoint": 768,"cols": 12,"margin": 40,"gutter": 20},
-                       {"state": "small","breakpoint": 1024,"cols": 12,"margin": 40,"gutter": 20},
-                       {"state": "medium","breakpoint": 1366,"cols": 16,"margin": 80,"gutter": 20},
-                       {"state": "large","breakpoint": 1920,"cols": 16,"margin": 80,"gutter": 40},
-                       {"state": "ultrahd","breakpoint": 10000,"cols": 16,"margin": 120,"gutter": 40}]);
-
       app = angular
               .module(appName, [
                 'ui.router',
--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
-/*global head, respond, System */
+/*global head, res, System */
+var respond;
 (function( window, head ) {
   "use strict";
 
@@ -18,6 +19,15 @@
       'lib/require.js'
     )
     .ready("ALL", function() {
+
+      // create an instance of res.js globally
+      respond = new res([{"state": "mobile","breakpoint": 320,"cols": 4,"margin": 10,"gutter": 10},
+                       {"state": "phablet","breakpoint": 568,"cols": 4,"margin": 10,"gutter": 10},
+                       {"state": "tablet","breakpoint": 768,"cols": 12,"margin": 40,"gutter": 20},
+                       {"state": "small","breakpoint": 1024,"cols": 12,"margin": 40,"gutter": 20},
+                       {"state": "medium","breakpoint": 1366,"cols": 16,"margin": 80,"gutter": 20},
+                       {"state": "large","breakpoint": 1920,"cols": 16,"margin": 80,"gutter": 40},
+                       {"state": "ultrahd","breakpoint": 10000,"cols": 16,"margin": 120,"gutter": 40}]);
       // Main RequireJS Config
       require.config({
       //  baseUrl:'./',
